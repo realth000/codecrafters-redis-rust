3890db280c5e578ce08e3c060075be7973def093
realth000 <a598919335@gmail.com>
2025-09-03 17:09:53 +0800

    respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(mut stream) => loop {
+                let mut buf = [0u8; 1024];
+                let n = stream.read(&mut buf).expect("failed to recv");
+                if n == 0 {
+                    println!("connection closed");
+                    break;
+                }
                 stream.write(b"+PONG\r\n").expect("failed to respond");
                 println!("accepted new connection");
-            }
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
