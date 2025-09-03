a166ffbf46decd29b8ba151c54071bec16af0f98
realth000 <a598919335@gmail.com>
2025-09-03 17:04:39 +0800

    respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                stream.write(b"+PONG\r\n").expect("failed to respond");
                 println!("accepted new connection");
             }
             Err(e) => {
