86f1075659eb647ed26e6e418d2d92e2776da072
realth000 <a598919335@gmail.com>
2025-09-09 16:18:59 +0800

    configure listening port

--- codecrafters-redis/src/main.rs
+++ codecrafters-redis/src/main.rs
@@ -13,7 +13,16 @@ mod transaction;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let server = RedisServer::new(Ipv4Addr::new(127, 0, 0, 1), 6379);
+    let args = std::env::args().collect::<Vec<_>>();
+    let mut port = 6379;
+    for w in args.windows(2) {
+        match w[0].as_str() {
+            "--port" => port = w[1].parse::<u16>().context("invalid port")?,
+            _ => continue,
+        }
+    }
+
+    let server = RedisServer::new(Ipv4Addr::new(127, 0, 0, 1), port);
     server.serve().await.context("when running server")?;
     Ok(())
 }
